e211874e87465606981263984582d5eb5e20be9d
Archie Baer <archie@abaer.dev>
2020-01-22 17:53:38 +0000

    Feed endpoint

--- api/_.js
+++ api/_.js
@@ -2,10 +2,19 @@ const express = require("express");
 const sessionAuth = require("../util/sessionAuth");
 
 const router = express.Router();
+
+//Sign in
 router.post("/login", require("./login"));
+
+//Basic User Info
 router.get("/me", sessionAuth, require("./me"));
 router.post("/me", sessionAuth, require("./updateBasicProfile"));
+
+//OAuth Authorization
 router.get("/application/:application", sessionAuth, require("./application"));
 router.post("/authorize", sessionAuth, require("./authorize"));
+
+//Feed
+router.get("/feed", sessionAuth, require("./feed"));
 
 module.exports = router;
---
+++ api/feed.js
@@ -0,0 +1,26 @@
+const db = require("../util/mongo");
+
+module.exports = async (req, res) => {
+    
+    //Get Posts
+    const posts = await Promise.all((
+        await db("posts").find({feeds: req.user._id}).sort({date: -1}).limit(500).toArray()
+    ).map(async (p) => {
+        const author = await db("accounts").findOne({_id: p.author});
+        return {
+            id: p.shortId,
+            author: {
+                id: author._id,
+                username: author.username,
+                nickname: author.nickname
+            },
+            content: p.content,
+            date: p.date,
+            liked: p.likes.includes(req.user._id),
+            boosted: p.likes.includes(req.user._id)
+        };
+    }));
+
+    res.json(posts);
+
+};
