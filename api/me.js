2d7eecd58b25655767246316dd9a2fd273303dd3
Archie Baer <archie@abaer.dev>
2020-02-01 23:24:49 +0000

    More user info for /me

--- api/me.js
+++ api/me.js
@@ -5,6 +5,19 @@ module.exports = async (req, res) => {
         name: req.user.name,
         nickname: req.user.nickname,
         about: req.user.about,
-        private: req.user.private
+        private: req.user.private,
+        au: req.user.au,
+        plus: {
+            status: plusStatus(req.user.premium),
+            ...req.user.premium
+        },
+        rubies: req.user.rubies,
+        createdAt: req.user.createdAt
     });
+};
+
+const plusStatus = plus => {
+    if (!plus) return "inactive";
+    if (new Date(plus.until).getTime() < new Date().getTime()) return "expired";
+    return "active";
 };
