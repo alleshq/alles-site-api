f8819e643f9b2e978e6710839270027a7a11e80d
Archie Baer <archie@abaer.dev>
2020-02-02 13:29:59 +0000

    Dev env var affects avatar storage path

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Alles Site API
-This is the internal api for v4 of the main Alles site.
+This is the internal api for v4 of the main Alles site. In order for some parts to function correctly in development, `NODE_ENV` must be set to `development`.
---
+++ config.js
@@ -0,0 +1,33 @@
+const dev = process.env.NODE_ENV === "development";
+
+module.exports = {
+    dev,
+    sessionTokenLifespan: "6h",
+    validScopes: [
+        "basic-profile",
+        "team-list"
+    ],
+    inputBounds: {
+        name: {
+            min: 1,
+            max: 50
+        },
+        nickname: {
+            min: 1,
+            max: 50
+        },
+        about: {
+            min: 1,
+            max: 125
+        },
+        password: {
+            min: 6,
+            max: 128
+        }
+    },
+    fileUploadSize: 50,
+    avatar: {
+        maxSize: 200,
+        storage: dev ? "../avatars/data" : "/avatars"
+    }
+};
