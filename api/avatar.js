597d1a897a24c6a33c1594d51b570cc55ca74726
Archie Baer <archie@abaer.dev>
2020-02-03 17:10:41 +0000

    Patched a bug

--- api/avatar.js
+++ api/avatar.js
@@ -3,7 +3,7 @@ const config = require("../config");
 const avatarTypes = ["image/jpeg", "image/png"];
 
 module.exports = async (req, res) => {
-    if (typeof req.files !== "object" || typeof req.files.avatar !== "object") return res.status(400).json({err: "fileUploadFailed"});
+    if (typeof req.files !== "object" || req.files === null || typeof req.files.avatar !== "object") return res.status(400).json({err: "fileUploadFailed"});
     const {avatar} = req.files;
     if (avatar.size > config.avatar.maxSize * 1024) return res.status(400).json({err: "avatarTooBig"}); //Avatar Size in Kilobytes
     if (!avatarTypes.includes(avatar.mimetype)) return res.status(400).json({err: "badFileType"});
