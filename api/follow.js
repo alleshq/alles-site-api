3e1059d4384df6ff4de716cd5cd6b1b234ab8eac
Archie Baer <archie@abaer.dev>
2020-02-07 18:59:15 +0000

    Users should not be able to follow themselves

--- api/follow.js
+++ api/follow.js
@@ -4,6 +4,9 @@ module.exports = async (req, res) => {
     const user = await db("accounts").findOne({_id: req.params.id});
     if (!user) return res.status(400).json({err: "invalidUser"});
 
+    //Same User
+    if (user._id === req.user._id) return res.status(400).json({err: "cannotFollowSelf"});
+
     //Already Followed
     if (user.followers.includes(req.user._id)) return res.json({});
 
