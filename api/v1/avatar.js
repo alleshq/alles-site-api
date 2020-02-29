cf525d2fe877fdbe90e0331e0525f16aef60aaf1
Archie Baer <archie@abaer.dev>
2020-02-29 13:48:15 +0000

    Fixed mimetype check

--- api/v1/avatar.js
+++ api/v1/avatar.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
     if (typeof req.files !== "object" || req.files === null || typeof req.files.avatar !== "object") return res.status(400).json({err: "fileUploadFailed"});
     const {avatar} = req.files;
     if (avatar.size > config.avatar.maxSize * 1024) return res.status(400).json({err: "avatarTooBig"}); //Avatar Size in Kilobytes
-    if (avatar.mimeType !== "image/png") return res.status(400).json({err: "badFileType"});
+    if (avatar.mimetype !== "image/png") return res.status(400).json({err: "badFileType"});
 
     const filePath = `${__dirname}/../../${config.avatar.storage}/users/${req.user.id}`;
     avatar.mv(filePath);
