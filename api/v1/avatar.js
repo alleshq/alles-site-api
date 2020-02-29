6aecb62021be72bfcd75ec992ed0e39361d18775
Archie Baer <archie@abaer.dev>
2020-02-29 13:11:35 +0000

    Only allows png images

--- api/v1/avatar.js
+++ api/v1/avatar.js
@@ -1,11 +1,10 @@
 const config = require("../../config");
-const avatarTypes = ["image/jpeg", "image/png"];
 
 module.exports = async (req, res) => {
     if (typeof req.files !== "object" || req.files === null || typeof req.files.avatar !== "object") return res.status(400).json({err: "fileUploadFailed"});
     const {avatar} = req.files;
     if (avatar.size > config.avatar.maxSize * 1024) return res.status(400).json({err: "avatarTooBig"}); //Avatar Size in Kilobytes
-    if (!avatarTypes.includes(avatar.mimetype)) return res.status(400).json({err: "badFileType"});
+    if (avatar.mimeType !== "image/png") return res.status(400).json({err: "badFileType"});
 
     const filePath = `${__dirname}/../../${config.avatar.storage}/users/${req.user.id}`;
     avatar.mv(filePath);
