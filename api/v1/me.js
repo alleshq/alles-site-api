685266af42d00024993b715445136930904b8a2c
Archie Baer <archie@abaer.dev>
2020-03-18 17:18:24 +0000

    Primary account in "me" endpoint

--- api/v1/me.js
+++ api/v1/me.js
@@ -1,4 +1,6 @@
 module.exports = async (req, res) => {
+	const primary = await req.user.getPrimary();
+
 	res.json({
 		id: req.user.id,
 		username: req.user.username,
@@ -9,6 +11,13 @@ module.exports = async (req, res) => {
 		au: 0,
 		plus: req.user.plus,
 		rubies: req.user.rubies,
-		createdAt: req.user.createdAt
+		createdAt: req.user.createdAt,
+		primary: primary
+			? {
+					id: primary.id,
+					username: primary.username,
+					name: primary.name
+			  }
+			: null
 	});
 };
